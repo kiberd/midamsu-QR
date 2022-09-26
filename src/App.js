b4c711701c2b6c47cd697c131ff26418533cc91d
unknown <kiberd22@gmail.com>
2022-09-26 11:41:43 +0900

    1차 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Switch } from '@headlessui/react'
 
 function App() {
+
+  const [isCn, setIsCn] = useState(true);
+  const [src, setSrc] = useState("/img_cn.jpg");
+
+  const handleClick = () => {
+    setIsCn(!isCn);
+    isCn ? setSrc("/img_jp.jpg") : setSrc("/img_cn.jpg");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex-col justify-center items-center w-full">
+
+        <div className="flex items-center mt-4 ml-[10vw] font-bold" onClick={handleClick}>
+
+          <Switch
+            checked={isCn}
+            onChange={setIsCn}
+            className={`${isCn ? 'bg-teal-700' : 'bg-teal-900'
+              } relative inline-flex h-6 w-11 items-center rounded-full`}
+          >
+            <span className="sr-only">Enable notifications</span>
+            <span
+              className={`${isCn ? 'translate-x-6' : 'translate-x-1'
+                } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+            />
+          </Switch>
+          <button className="ml-2">
+            {
+              isCn ? "일본어" : "중국어"
+            }
+          </button>
+
+        </div>
+        <div className="flex justify-center ml-4 items-center">
+
+          <img src={process.env.PUBLIC_URL + src} />
+        </div>
+      </div>
+    </>
   );
 }
 
